387683ddbc89574af1e3ea0475c2b5d619215498
Oladiran Segun Solomon <sheghunoladiran9@gmail.com>
2020-03-07 11:07:29 +0100

    Added configuration files

---
+++ index.ts
@@ -0,0 +1,20 @@
+/**
+ * @class Paystack
+ * @author sheghun {@link https://github.com/sheghun}
+ * Paystack wrapper for communicating with the  paystack's api
+ * {@link https://github.com/sheghun/paystack2}
+ *    **********
+ *
+ * You can also import the version you need by importing the subdirectory of the version directly
+ * e.g require('paystack2/v1') or require('paystack2/v2')
+ */
+class Paystack {
+    /**
+     * @param {string} sk - secret key
+     * @param {number} v - version of api defaults to 1
+     *
+     */
+    constructor(sk: string,  v: number) {
+
+    }
+}
