a902a9650710fc295739a63081ae88667de3cacd
Oladiran Segun Solomon <sheghunoladiran9@gmail.com>
2020-03-07 20:43:13 +0100

    Declared subaccount functions

---
+++ resources/subaccount.ts
@@ -0,0 +1,18 @@
+/**
+ * @class Subaccount
+ *  Subaccount resource
+ * {@link https://github.com/sheghun/paystack2}
+ *    **********
+ *
+ */
+class Subaccount {
+    static endpoint = '/subaccount';
+
+    static async createSubaccount() {}
+
+    static async listSubaccount() {}
+
+    static async fetchSubaccount() {}
+
+    static async updateSubaccount() {}
+}
